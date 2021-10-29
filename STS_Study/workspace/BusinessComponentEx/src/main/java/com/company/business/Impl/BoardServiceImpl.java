175fefb1092cce2a9f1b784ad8829903beb266fb
ino5 <chero77@naver.com>
2021-10-29 16:19:36 +0900

    update BusinessComponentEx project (aop). 21-10-29 오후 수업

--- STS_Study/workspace/BusinessComponentEx/src/main/java/com/company/business/Impl/BoardServiceImpl.java
+++ STS_Study/workspace/BusinessComponentEx/src/main/java/com/company/business/Impl/BoardServiceImpl.java
@@ -12,38 +12,67 @@ import com.company.business.board.BoardService;
  * 어느 클래스 선언부 위에 @Service 어노테이션을 붙이면
  * 이 클래스는 비즈니스 로직을 처리하는 클래스라고 스프링 컨테이너에게 알려준다.
  */
+import com.company.business.common.LogAdvice;
 
 @Service("boardService")
 public class BoardServiceImpl implements BoardService{
 	
 	@Autowired
 	private BoardDAO boardDAO;
+	// 추가
+	private LogAdvice log;
+	
+	// 생성자
+	public BoardServiceImpl() {
+		log = new LogAdvice();
+	}
 	
 	@Override
 	public List<BoardDO> getBoardList(BoardDO boardDO) {
+		// 추가
+//		log.printLog(); // 공통부분
+		// 보안처리 소스
+		// 트랜잭션 처리 소스
+		
 		return boardDAO.getBoardList(boardDO);
 	}
 
 	@Override
 	public BoardDO getBoard(BoardDO boardDO) {
-		// TODO Auto-generated method stub
+//		log.printLog(); // 공통부분
+		// 보안처리 소스
+		// 트랜잭션 처리 소스
+		
+		// return boardDAO.getBoard(boardDO);
 		return null;
 	}
 
 	@Override
 	public void insertBoard(BoardDO boardDO) {
+//		log.printLog(); // 공통부분
+		// 보안처리 소스
+		// 트랜잭션 처리 소스
+		
 		boardDAO.insertBoard(boardDO);
 	}
 
 	@Override
 	public void updateBoard(BoardDO boardDO) {
-		// TODO Auto-generated method stub
+//		log.printLog(); // 공통부분
+		// 보안처리 소스
+		// 트랜잭션 처리 소스
+		
+		// return boardDAO.updateBoard(boardDO);
 		
 	}
 
 	@Override
 	public void deleteBoard(BoardDO boardDO) {
-		// TODO Auto-generated method stub
+//		log.printLog(); // 공통부분
+		// 보안처리 소스
+		// 트랜잭션 처리 소스
+		
+		// return boardDAO.deleteBoard(boardDO);
 		
 	}
 
